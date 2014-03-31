abba9f9a56d0d07302236499356dbde181a7dd9a
Iulian Margarintescu <iulian@recognos.com>
2014-03-31 17:47:17 +0300

    nuget packaging

---
+++ Publishing/create-nuget.bat
@@ -0,0 +1,1 @@
+..\.nuget\NuGet.exe pack Metrics.Net.nuspec
--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Release"
-copy .\bin\release\metrics.dll .\release\bin\
-copy .\bin\release\metrics.xml .\release\bin\
-copy .\bin\release\metrics.pdb .\release\bin\
-copy .\bin\release\nancy.metrics.dll .\release\bin\
-copy .\bin\release\nancy.metrics.xml .\release\bin\
-copy .\bin\release\nancy.metrics.pdb .\release\bin\
+
+md .\release\lib
+md .\release\lib\net451
+
+copy .\bin\release\metrics.dll .\release\lib\net451\
+copy .\bin\release\metrics.xml .\release\lib\net451\
+copy .\bin\release\metrics.pdb .\release\lib\net451\
+copy .\bin\release\nancy.metrics.dll .\release\lib\net451\
+copy .\bin\release\nancy.metrics.xml .\release\lib\net451\
+copy .\bin\release\nancy.metrics.pdb .\release\lib\net451\
