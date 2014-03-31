749d5cdaae07f1f63f24ac76be9c4031e9633e1c
Iulian Margarintescu <iulian@recognos.com>
2014-03-31 17:36:02 +0300

    add build script. bump assembly version

---
+++ build.bat
@@ -0,0 +1,7 @@
+c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Release"
+copy .\bin\release\metrics.dll .\release\bin\
+copy .\bin\release\metrics.xml .\release\bin\
+copy .\bin\release\metrics.pdb .\release\bin\
+copy .\bin\release\nancy.metrics.dll .\release\bin\
+copy .\bin\release\nancy.metrics.xml .\release\bin\
+copy .\bin\release\nancy.metrics.pdb .\release\bin\
