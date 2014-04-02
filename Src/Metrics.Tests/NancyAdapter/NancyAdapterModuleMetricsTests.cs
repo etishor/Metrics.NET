2bacdc1684e98ed42c7abcb162bfbf29e460703c
Iulian Margarintescu <iulian@recognos.com>
2014-04-02 20:19:15 +0300

    Tests for nancy adapter

---
+++ Src/Metrics.Tests/NancyAdapter/NancyAdapterModuleMetricsTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Metrics.Core;
+using Metrics.Tests.TestUtils;
+using Metrics.Utils;
+using Nancy;
+using Nancy.Metrics;
+using Nancy.Testing;
+using Xunit;
+
+namespace Metrics.Tests.NancyAdapter
+{
+    public class NancyAdapterModuleMetricsTests
+    {
+        public class TestModule : NancyModule
+        {
+            public TestModule(Clock.TestClock clock)
+                : base("/test")
+            {
+                this.MetricForRequestTime("Action", "Get", "/action");
+
+                Get["/action"] = _ =>
+                {
+                    clock.Advance(TimeUnit.Milliseconds, 100);
+                    return Response.AsText("response");
+                };
+            }
+        }
+
+        [Fact]
+        public void NancyMetricsShouldBeAbleMonitorModuleRequest()
+        {
+            Clock.TestClock clock = new Clock.TestClock();
+            Timer timer = new TimerMetric(SamplingType.LongTerm, clock);
+            NancyMetrics.Configure(new TestRegistry(timer));
+
+            var browser = new Browser(with =>
+            {
+                with.Module(new TestModule(clock));
+            });
+
+            var response = browser.Get("/test/action");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var value = timer.Value;
+            value.Rate.Count.Should().Be(1);
+            value.Histogram.Count.Should().Be(1);
+            value.Histogram.Max.Should().Be(TimeUnit.Milliseconds.ToNanoseconds(100));
+        }
+    }
+}
---
+++ Src/Metrics.Tests/TestUtils/TestRegistry.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Metrics.Tests.TestUtils
+{
+    public class TestRegistry : MetricsRegistry
+    {
+        public TestRegistry()
+        {
+
+        }
+
+        public TestRegistry(Timer timer)
+        {
+            this.TimerInstance = timer;
+        }
+
+        public string Name { get { return "test"; } }
+
+        public Timer TimerInstance { get; set; }
+
+        public Func<string, Func<string>, Unit, Gauge> GaugeBuilder { get; set; }
+        public Func<string, Unit, Counter> CounterBuilder { get; set; }
+        public Func<string, Unit, TimeUnit, Meter> MeterBuilder { get; set; }
+        public Func<string, Unit, SamplingType, Histogram> HistogramBuilder { get; set; }
+        public Func<string, Unit, SamplingType, TimeUnit, TimeUnit, Timer> TimerBuilder { get; set; }
+
+        public Gauge Gauge(string name, Func<string> valueProvider, Unit unit)
+        {
+            return GaugeBuilder(name, valueProvider, unit);
+        }
+
+        public Gauge Gauge(string name, Func<Gauge> gauge, Unit unit)
+        {
+            return gauge();
+        }
+
+        public Counter Counter(string name, Unit unit)
+        {
+            return CounterBuilder(name, unit);
+        }
+
+        public Meter Meter(string name, Unit unit, TimeUnit rateUnit)
+        {
+            return MeterBuilder(name, unit, rateUnit);
+        }
+
+        public Histogram Histogram(string name, Unit unit, SamplingType samplingType)
+        {
+            return HistogramBuilder(name, unit, samplingType);
+        }
+
+        public Timer Timer(string name, Unit unit, SamplingType samplingType, TimeUnit rateUnit, TimeUnit durationUnit)
+        {
+            return TimerInstance != null ? TimerInstance : TimerBuilder(name, unit, samplingType, rateUnit, durationUnit);
+        }
+
+        public IEnumerable<Meta.GaugeMeta> Gauges
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerable<Meta.CounterMeta> Counters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerable<Meta.MeterMeta> Meters
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerable<Meta.HistogramMeta> Histograms
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerable<Meta.TimerMeta> Timers
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+
+        public void ClearAllMetrics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
