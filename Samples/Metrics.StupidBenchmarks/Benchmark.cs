390403e99b8976c87a38816fe62c0fae6878b1cc
Iulian Margarintescu <iulian@recognos.com>
2014-04-03 14:58:50 +0300

    utility for measuring the effects of changes

--- Samples/Metrics.StupidBenchmarks/Benchmark.cs
+++ Samples/Metrics.StupidBenchmarks/Benchmark.cs
@@ -1,50 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Metrics.Core;
 
 namespace Metrics.StupidBenchmarks
 {
     public static class Benchmark
     {
-        public static void Run()
+        public struct Result
         {
-            const int threadCount = 32;
-            const long iterations = 1000000;// *1000;
-
-
-            Timer timer = new TimerMetric();
-
-            for (int i = 0; i < iterations; i++ )
+            public TimeSpan Elapsed;
+            public long Total;
+            public double PerSecond
             {
-                timer.Time(() => { Math.Sin(i * i); });
+                get
+                {
+                    return Total / Elapsed.TotalSeconds;
+                }
             }
 
+            public override string ToString()
+            {
+                return string.Format("Runs\t{0}\nTotal\t{1}\nRate\t{2}", Total, Elapsed, PerSecond);
+            }
+        }
+
+        public static Result Run(Action<int, long> action, int threadCount = 8, long iterations = 1 * 1000 * 1000)
+        {
+#if DEBUG
+            Console.WriteLine("DEBUG MODE - results are relevant");
+#endif
+            // warm - up
+            for (int i = 0; i < 100; i++) { action(0, 0); }
+
             List<Thread> threads = new List<Thread>();
+            TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
 
             for (int i = 0; i < threadCount; i++)
             {
+                var x = i;
                 threads.Add(new Thread(s =>
-                {
-                    Thread.Sleep(100);
-                    for (long j = 0; j < iterations; j++)
                     {
-                        timer.Time(() => { Math.Sin(i * j); });
-                    }
-                }));
+                        tcs.Task.Wait();
+                        for (long j = 0; j < iterations; j++)
+                            action(x, j);
+                    }));
             }
+            // all thread will be waiting on tcs
+            threads.ForEach(t => t.Start());
 
             var w = Stopwatch.StartNew();
-            threads.ForEach(t => t.Start());
+            tcs.SetResult(0);
             threads.ForEach(t => t.Join());
             var elapsed = w.Elapsed;
-            Console.WriteLine(elapsed);
-            Console.WriteLine(threadCount * iterations / elapsed.TotalSeconds);
-
+            return new Result { Elapsed = elapsed, Total = threadCount * iterations };
         }
     }
 }
--- Samples/Metrics.StupidBenchmarks/Program.cs
+++ Samples/Metrics.StupidBenchmarks/Program.cs
@@ -1,15 +1,48 @@
 ﻿
 using System;
+using Metrics.Core;
+using Metrics.Utils;
 namespace Metrics.StupidBenchmarks
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Benchmark.Run();
+            Clock.TestClock clock = new Clock.TestClock();
+            Timer timer = new TimerMetric(SamplingType.FavourRecent, clock);
+
+            Counter counter = new CounterMetric();
+            Meter meter = new MeterMetric(clock);
+
+            for (int i = 1; i < 8; i++)
+            {
+                //TestMeter(clock, meter, i);
+                //TestCounter(counter, i);
+                TestTimer(clock, timer, i);
+            }
 
             Console.WriteLine("done");
             Console.ReadKey();
+        }
+
+        private static void TestMeter(Clock.TestClock clock, Meter meter, int threadCount)
+        {
+            var meterResult = Benchmark.Run((i, l) => { meter.Mark(i); clock.Advance(TimeUnit.Milliseconds, l); },
+                iterations: 50000, threadCount: threadCount);
+            Console.WriteLine("{0}\t{1}", threadCount, meterResult.PerSecond);
+        }
+
+        private static void TestCounter(Counter counter, int threadCount)
+        {
+            var counterResult = Benchmark.Run((i, l) => counter.Increment(l), iterations: 100000, threadCount: threadCount);
+            Console.WriteLine("{0}\t{1}", threadCount, counterResult.PerSecond);
+        }
+
+        private static void TestTimer(Clock.TestClock clock, Timer timer, int threadCount)
+        {
+            var timerResult = Benchmark.Run((i, l) => { using (timer.NewContext()) { clock.Advance(TimeUnit.Milliseconds, l); } },
+                iterations: 100000, threadCount: threadCount);
+            Console.WriteLine("{0}\t{1}", threadCount, timerResult.PerSecond);
         }
     }
 }
