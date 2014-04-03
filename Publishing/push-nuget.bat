5fdf4c75e26bb6f7483a27f655caa68bb6181657
Iulian Margarintescu <iulian@recognos.com>
2014-04-03 15:09:39 +0300

    bump version and publish nuget

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,2 +1,2 @@
-..\.nuget\NuGet.exe push Metrics.NET.0.0.2-alpha.nupkg
-..\.nuget\NuGet.exe push NancyFx.Metrics.0.0.2-alpha.nupkg
+..\.nuget\NuGet.exe push Metrics.NET.0.0.3-alpha.nupkg
+..\.nuget\NuGet.exe push NancyFx.Metrics.0.0.3-alpha.nupkg
