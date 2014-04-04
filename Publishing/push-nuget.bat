3a7971a4fd759443ebb5f46eb03060c8f889acd6
Iulian Margarintescu <iulian@recognos.com>
2014-04-04 12:23:09 +0300

    fix build script to publish the correct assemblies

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,2 +1,2 @@
-..\.nuget\NuGet.exe push Metrics.NET.0.0.4-alpha.nupkg
-..\.nuget\NuGet.exe push NancyFx.Metrics.0.0.4-alpha.nupkg
+..\.nuget\NuGet.exe push Metrics.NET.0.0.5-alpha.nupkg
+..\.nuget\NuGet.exe push NancyFx.Metrics.0.0.5-alpha.nupkg
--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Release"
 
-md .\release\lib
-md .\release\lib\net451
+md .\Publishing\lib
+md .\Publishing\lib\net451
 
-copy .\bin\release\metrics.dll .\release\lib\net451\
-copy .\bin\release\metrics.xml .\release\lib\net451\
-copy .\bin\release\metrics.pdb .\release\lib\net451\
-copy .\bin\release\nancy.metrics.dll .\release\lib\net451\
-copy .\bin\release\nancy.metrics.xml .\release\lib\net451\
-copy .\bin\release\nancy.metrics.pdb .\release\lib\net451\
+copy .\bin\release\metrics.dll .\Publishing\lib\net451\
+copy .\bin\release\metrics.xml .\Publishing\lib\net451\
+copy .\bin\release\metrics.pdb .\Publishing\lib\net451\
+copy .\bin\release\nancy.metrics.dll .\Publishing\lib\net451\
+copy .\bin\release\nancy.metrics.xml .\Publishing\lib\net451\
+copy .\bin\release\nancy.metrics.pdb .\Publishing\lib\net451\
