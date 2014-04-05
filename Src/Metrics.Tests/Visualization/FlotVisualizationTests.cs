22f763a1b07d30a82b8fe888d823d8c7eff84266
Iulian Margarintescu <etishor@gmail.com>
2014-04-05 18:58:21 +0300

    add test for visualization app

---
+++ Src/Metrics.Tests/Visualization/FlotVisualizationTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using Metrics.Visualization;
+using Xunit;
+
+namespace Metrics.Tests.Visualization
+{
+    public class FlotVisualizationTests
+    {
+        [Fact]
+        public void CanReadAppFromResource()
+        {
+            FlotWebApp.GetFlotApp(new Uri("http://localhost/")).Should().NotBeEmpty();
+            FlotWebApp.GetFlotApp(new Uri("http://xxx/")).Should().Contain("http://xxx/");
+        }
+    }
+}
