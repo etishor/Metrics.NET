8c5c7a7599b5041e98745904379e6ee17619f522
Iulian Margarintescu <etishor@gmail.com>
2014-04-05 18:58:02 +0300

    embed visualization app

---
+++ Src/Metrics/Visualization/FlotWebApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace Metrics.Visualization
+{
+    public static class FlotWebApp
+    {
+        private static string ReadFromEmbededResource()
+        {
+            using (var stream = Assembly.GetAssembly(typeof(FlotWebApp)).GetManifestResourceStream("Metrics.Visualization.index.full.html.gz"))
+            using (var gzip = new GZipStream(stream, CompressionMode.Decompress))
+            using (var reader = new StreamReader(gzip))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        private static Lazy<string> htmlContent = new Lazy<string>(() => ReadFromEmbededResource());
+
+        public static string GetFlotApp(Uri metricsJsonEndpoint)
+        {
+            return htmlContent.Value.Replace("http://localhost:1234/metrics/json", metricsJsonEndpoint.ToString());
+        }
+    }
+}
