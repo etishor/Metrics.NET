0d09a82919c0e1fc252bd5b59046262e1d75cc36
Iulian Margarintescu <iulian@recognos.com>
2014-04-11 11:23:47 +0300

    cleanup sample

--- Samples/Metrics.SamplesConsole/Program.cs
+++ Samples/Metrics.SamplesConsole/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Metrics.Samples;
-using Metrics.Visualization;
 
 namespace Metrics.SamplesConsole
 {
@@ -12,25 +11,13 @@ namespace Metrics.SamplesConsole
 
             SampleMetrics.RunSomeRequests();
 
-            //Metric.Reports.PrintConsoleReport(TimeSpan.FromSeconds(1));
+            Metric.Reports.StoreCSVReports(@"c:\temp\reports\", TimeSpan.FromSeconds(1));
+            Metric.Reports.AppendToFile(@"C:\temp\reports\metrics.txt", TimeSpan.FromSeconds(1));
 
-            //var json = new RegistrySerializer().ValuesAsJson(Metric.Registry);
-            //var x = JsonConvert.DeserializeObject<dynamic>(json);
-            //Console.WriteLine(json);
+            Metric.Reports.StartHttpListener("http://localhost:1234/");
 
-            //Metric.Reports.StoreCSVReports(@"c:\temp\reports\", TimeSpan.FromSeconds(1));
-            //Metric.Reports.AppendToFile(@"C:\temp\reports\metrics.txt", TimeSpan.FromSeconds(1));
-
-            using (MetricsHttpListener http = new MetricsHttpListener("http://localhost:1234/"))
-            {
-                http.Start();
-                Console.WriteLine("done");
-                Console.ReadKey();
-                http.Stop();
-            }
-
-            //Console.WriteLine("done");
-            //Console.ReadKey();
+            Console.WriteLine("done");
+            Console.ReadKey();
         }
     }
 }
