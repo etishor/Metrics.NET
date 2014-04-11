ae5f08216b683f2a209118a87274c83a034470cb
Iulian Margarintescu <iulian@recognos.com>
2014-04-11 18:50:12 +0300

    add configuration for targeting 4.5 to support mono

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
+c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Debug"
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Release"
+c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoDebug"
+c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoRelease"
