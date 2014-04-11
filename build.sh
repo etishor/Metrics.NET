67bd3c99bbf68d0c2615ed73e6bf97c07eb1f0be
Iulian Margarintescu <etishor@gmail.com>
2014-04-11 22:15:40 +0300

    added build script for mono

---
+++ build.sh
@@ -0,0 +1,7 @@
+mono .nuget/NuGet.exe restore Metrics.sln 
+
+xbuild Metrics.Sln /p:Configuration="MonoDebug"
+xbuild Metrics.Sln /p:Configuration="MonoRelease"
+
+mono ./packages/xunit.runners.1.9.2/tools/xunit.console.clr4.exe ./bin/MonoDebug/Tests/Metrics.Tests.dll
+mono ./packages/xunit.runners.1.9.2/tools/xunit.console.clr4.exe ./bin/MonoRelease/Tests/Metrics.Tests.dll
