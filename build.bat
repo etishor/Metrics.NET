0044702db2fa426bf7c43d33cb4394a97732b709
Iulian Margarintescu <iulian@recognos.com>
2014-04-11 19:27:56 +0300

    add support for .net 4.0

--- build.bat
+++ build.bat
@@ -2,3 +2,5 @@ c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Confi
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Release"
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoDebug"
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoRelease"
+c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Net4.0Debug"
+c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Net4.0Release"
