292ab7f35934a4f8a9ab5ee2dd80881dc7549601
Iulian Margarintescu <etishor@gmail.com>
2014-04-11 21:59:32 +0300

    run unit tests on build

--- build.bat
+++ build.bat
@@ -4,3 +4,10 @@ c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Confi
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoRelease"
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Net4.0Debug"
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Net4.0Release"
+
+.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Debug\Tests\Metrics.Tests.dll
+.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Release\Tests\Metrics.Tests.dll
+.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\MonoDebug\Tests\Metrics.Tests.dll
+.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\MonoRelease\Tests\Metrics.Tests.dll
+.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Net4.0Debug\Tests\Metrics.Tests.dll
+.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Net4.0Release\Tests\Metrics.Tests.dll
