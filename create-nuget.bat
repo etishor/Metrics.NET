7aa5aec808992b6053366747224f846403fdc5e0
Iulian Margarintescu <iulian@recognos.com>
2014-04-11 19:39:15 +0300

    add 4.5 & 4.0 targets to nuget packages

--- create-nuget.bat
+++ create-nuget.bat
@@ -4,13 +4,32 @@ call build.bat
 
 md .\Publishing\lib
 md .\Publishing\lib\net451
+md .\Publishing\lib\net45
+md .\Publishing\lib\net40
 
 copy .\bin\release\metrics.dll .\Publishing\lib\net451\
 copy .\bin\release\metrics.xml .\Publishing\lib\net451\
 copy .\bin\release\metrics.pdb .\Publishing\lib\net451\
+
+copy .\bin\MonoRelease\metrics.dll .\Publishing\lib\net45\
+copy .\bin\MonoRelease\metrics.xml .\Publishing\lib\net45\
+copy .\bin\MonoRelease\metrics.pdb .\Publishing\lib\net45\
+
+copy .\bin\Net4.0Release\metrics.dll .\Publishing\lib\net40\
+copy .\bin\Net4.0Release\metrics.xml .\Publishing\lib\net40\
+copy .\bin\Net4.0Release\metrics.pdb .\Publishing\lib\net40\
+
 copy .\bin\release\nancy.metrics.dll .\Publishing\lib\net451\
 copy .\bin\release\nancy.metrics.xml .\Publishing\lib\net451\
 copy .\bin\release\nancy.metrics.pdb .\Publishing\lib\net451\
+
+copy .\bin\MonoRelease\nancy.metrics.dll .\Publishing\lib\net45\
+copy .\bin\MonoRelease\nancy.metrics.xml .\Publishing\lib\net45\
+copy .\bin\MonoRelease\nancy.metrics.pdb .\Publishing\lib\net45\
+
+copy .\bin\Net4.0Release\nancy.metrics.dll .\Publishing\lib\net40\
+copy .\bin\Net4.0Release\nancy.metrics.xml .\Publishing\lib\net40\
+copy .\bin\Net4.0Release\nancy.metrics.pdb .\Publishing\lib\net40\
 
 .\.nuget\NuGet.exe pack .\Publishing\Metrics.Net.nuspec -OutputDirectory .\Publishing
 .\.nuget\NuGet.exe pack .\Publishing\NancyFx.Metrics.nuspec -OutputDirectory .\Publishing
