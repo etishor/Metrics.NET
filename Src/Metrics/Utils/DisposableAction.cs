a02ede81c760cccbda3de531a5f7c5754dcc7d0d
Iulian Margarintescu <etishor@gmail.com>
2014-04-12 21:40:03 +0300

    add utility struct for executing an action at dispose

---
+++ Src/Metrics/Utils/DisposableAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Metrics.Utils
+{
+    /// <summary>
+    /// Utility structure that executed an action when it is disposed;
+    /// </summary>
+    public struct DisposableAction : IDisposable
+    {
+        private readonly Action action;
+        private bool disposed;
+
+        /// <summary>
+        /// Creates a new instance that will execute <paramref name="action"/> when it is disposed
+        /// </summary>
+        /// <param name="action">Action to execute when struct is disposed.</param>
+        public DisposableAction(Action action)
+        {
+            this.action = action;
+            this.disposed = false;
+        }
+
+        /// <summary>
+        /// Executed the action this instance has been constructed with.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!this.disposed)
+            {
+                this.action();
+            }
+            this.disposed = true;
+        }
+    }
+}
