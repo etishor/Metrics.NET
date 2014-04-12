acc22e92becbd4ffce6d7c9edd27c29462881b3a
Iulian Margarintescu <etishor@gmail.com>
2014-04-12 19:28:19 +0300

    stop build on first error

--- build.bat
+++ build.bat
@@ -1,13 +1,35 @@
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Debug"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Release"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoDebug"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoRelease"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Net4.0Debug"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Net4.0Release"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 .\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Debug\Tests\Metrics.Tests.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 .\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Release\Tests\Metrics.Tests.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 .\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\MonoDebug\Tests\Metrics.Tests.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 .\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\MonoRelease\Tests\Metrics.Tests.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 .\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Net4.0Debug\Tests\Metrics.Tests.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 .\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Net4.0Release\Tests\Metrics.Tests.dll
+if %errorlevel% neq 0 exit /b %errorlevel%
