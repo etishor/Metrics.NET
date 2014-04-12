995c610dcca9b1dfd74af355ea980fcaf5e0f241
Iulian Margarintescu <etishor@gmail.com>
2014-04-12 19:26:32 +0300

    mark clock implementations as sealed

--- Src/Metrics/Utils/Clock.cs
+++ Src/Metrics/Utils/Clock.cs
@@ -5,12 +5,12 @@ namespace Metrics.Utils
 {
     public abstract class Clock
     {
-        private class SystemClock : Clock
+        private sealed class SystemClock : Clock
         {
             public override long Nanoseconds { get { return DateTime.UtcNow.Ticks * 100L; } }
         }
 
-        public class TestClock : Clock
+        public sealed class TestClock : Clock
         {
             private long nanoseconds = 0;
             public override long Nanoseconds { get { return this.nanoseconds; } }
