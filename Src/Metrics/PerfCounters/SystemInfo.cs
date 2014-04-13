b0eb72a5ad36fdd61391812bc3dc5efb5fec6d27
Iulian Margarintescu <etishor@gmail.com>
2014-04-13 17:15:26 +0300

    remove old system info string gauges

--- Src/Metrics/PerfCounters/SystemInfo.cs
+++ Src/Metrics/PerfCounters/SystemInfo.cs
@@ -10,21 +10,10 @@ namespace Metrics.PerfCounters
 
         public void Register()
         {
-            //Register("MachineName", () => Environment.MachineName);
-            //Register("Processor Count", () => Environment.ProcessorCount.ToString());
-            //Register("OperatingSystem", () => GetOSVersion());
-            //Register("NETVersion", () => Environment.Version.ToString());
-            //Register("CommandLine", () => Environment.CommandLine.Replace("\"",string.Empty).Replace("\\","/"));
-
             Register("AvailableRAM", () => new PerformanceCounterGauge("Memory", "Available MBytes"), Unit.Custom("Mb"));
             Register("CPU Usage", () => new PerformanceCounterGauge("Processor", "% Processor Time", TotalInstance), Unit.Custom("%"));
             Register("Disk Writes/sec", () => new DerivedGauge(new PerformanceCounterGauge("PhysicalDisk", "Disk Reads/sec", TotalInstance), f => f / 1024), Unit.Custom("kb/s"));
             Register("Disk Reads/sec", () => new DerivedGauge(new PerformanceCounterGauge("PhysicalDisk", "Disk Writes/sec", TotalInstance), f => f / 1024), Unit.Custom("kb/s"));
-        }
-
-        private static string GetOSVersion()
-        {
-            return string.Format("{0} {1}", Environment.OSVersion.VersionString, Environment.Is64BitOperatingSystem ? "64bit" : "32bit");
         }
     }
 }
