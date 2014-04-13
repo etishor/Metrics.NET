77bda6f67063698b79d17fb14c326e0bb2719f80
Iulian Margarintescu <etishor@gmail.com>
2014-04-13 20:08:08 +0300

    initial health checks implementation

---
+++ Src/Metrics/Core/HealthCheck.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Metrics.Core
+{
+    public sealed class HealthCheck
+    {
+        private readonly Func<HealthCheckResult> check;
+
+        public HealthCheck(string name, Action check)
+            : this(name, () => { check(); return string.Empty; })
+        { }
+
+        public HealthCheck(string name, Func<string> check)
+            : this(name, MapFunction(name, check))
+        { }
+
+        public HealthCheck(string name, Func<HealthCheckResult> check)
+        {
+            this.Name = name;
+            this.check = check;
+        }
+
+        public string Name { get; private set; }
+
+        private static Func<HealthCheckResult> MapFunction(string name, Func<string> check)
+        {
+            return () =>
+            {
+                try
+                {
+                    var result = check();
+                    return HealthCheckResult.Healthy(name, result);
+                }
+                catch (Exception x)
+                {
+                    return HealthCheckResult.Unhealthy(name, x);
+                }
+            };
+        }
+
+        public HealthCheckResult Execute()
+        {
+            try
+            {
+                return this.check();
+            }
+            catch (Exception x)
+            {
+                return HealthCheckResult.Unhealthy(this.Name, x);
+            }
+        }
+    }
+}
---
+++ Src/Metrics/HealthChecks.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Metrics.Core;
+
+namespace Metrics
+{
+    public struct HealthStatus
+    {
+        public readonly bool IsHealty;
+        public readonly HealthCheckResult[] Results;
+
+        public HealthStatus(bool isHealthy, IEnumerable<HealthCheckResult> results)
+        {
+            this.IsHealty = isHealthy;
+            this.Results = results.ToArray();
+        }
+    }
+
+    public class HealthChecks
+    {
+        private readonly ConcurrentDictionary<string, HealthCheck> checks = new ConcurrentDictionary<string, HealthCheck>();
+
+        public void RegisterHealthCheck(string name, Action check)
+        {
+            RegisterHealthCheck(new HealthCheck(name, check));
+        }
+
+        public void RegisterHealthCheck(string name, Func<string> check)
+        {
+            RegisterHealthCheck(new HealthCheck(name, check));
+        }
+
+        public void RegisterHealthCheck(string name, Func<HealthCheckResult> check)
+        {
+            RegisterHealthCheck(new HealthCheck(name, check));
+        }
+
+        public void RegisterHealthCheck(HealthCheck check)
+        {
+            this.checks.AddOrUpdate(check.Name, check, (n, c) => check);
+        }
+
+        public IEnumerable<HealthCheckResult> GetStatus()
+        {
+            return this.checks.Values.Select(v => v.Execute()).OrderBy(r => r.Name);
+        }
+    }
+}
