c156b0aab799f30664daabd0ac7bb19b08accee5
Iulian Margarintescu <iulian@recognos.com>
2014-04-14 12:04:18 +0300

    refactor the build script a bit

--- build.bat
+++ build.bat
@@ -1,35 +1,40 @@
-c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Debug"
+.nuget\NuGet.exe restore Metrics.sln
+
+set MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+set XUNIT=".\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+
+%MSBUILD% Metrics.Sln /p:Configuration="Debug"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Release"
+%MSBUILD% Metrics.Sln /p:Configuration="Release"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoDebug"
+%MSBUILD% Metrics.Sln /p:Configuration="MonoDebug"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="MonoRelease"
+%MSBUILD% Metrics.Sln /p:Configuration="MonoRelease"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Net4.0Debug"
+%MSBUILD% Metrics.Sln /p:Configuration="Net4.0Debug"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-c:\WINDOWS\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Metrics.Sln /p:Configuration="Net4.0Release"
+%MSBUILD% Metrics.Sln /p:Configuration="Net4.0Release"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Debug\Tests\Metrics.Tests.dll
+%XUNIT% .\bin\Debug\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Release\Tests\Metrics.Tests.dll
+%XUNIT% .\bin\Release\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\MonoDebug\Tests\Metrics.Tests.dll
+%XUNIT% .\bin\MonoDebug\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\MonoRelease\Tests\Metrics.Tests.dll
+%XUNIT% .\bin\MonoRelease\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Net4.0Debug\Tests\Metrics.Tests.dll
+%XUNIT% .\bin\Net4.0Debug\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-.\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\bin\Net4.0Release\Tests\Metrics.Tests.dll
+%XUNIT% .\bin\Net4.0Release\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
