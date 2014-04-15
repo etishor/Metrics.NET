2c9e76fd42580a5bb1849dfbe9b904e97d1376cd
Iulian Margarintescu <iulian@recognos.com>
2014-04-15 12:13:04 +0300

    fix build

--- Samples/Metrics.StupidBenchmarks/Program.cs
+++ Samples/Metrics.StupidBenchmarks/Program.cs
@@ -1,6 +1,4 @@
-﻿
-using System;
-using Metrics.Core;
+﻿using Metrics.Core;
 namespace Metrics.StupidBenchmarks
 {
     class Program
@@ -10,13 +8,9 @@ namespace Metrics.StupidBenchmarks
             //FixedTimeBenchmark.Run<CounterMetric>(m => m.Increment());
             //FixedTimeBenchmark.Run<MeterMetric>(m => m.Mark());
             //FixedTimeBenchmark.Run<HistogramMetric>(m => m.Update(1));
-            FixedTimeBenchmark.Run<TimerMetric>(m => { using (m.NewContext()) { } });
+            FixedTimeBenchmark.Run<TimerMetric>(m => { using (m.NewContext()) { } }, maxThreads: 32, seconds: 8);
 
             //FixedIterationsBenchmark.Run<TimerMetric>(m => { using (m.NewContext()) { } }, 1000 * 1000);
-#if DEBUG
-            Console.WriteLine("done");
-            Console.ReadKey();
-#endif
         }
     }
 }
