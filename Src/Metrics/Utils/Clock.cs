4b20c893598bd48e3bdb2a2698dfa8d6bdb1952d
Iulian Margarintescu <etishor@gmail.com>
2014-04-18 19:29:43 +0300

    add method to report local time

--- Src/Metrics/Utils/Clock.cs
+++ Src/Metrics/Utils/Clock.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Diagnostics;
 
 namespace Metrics.Utils
 {
@@ -7,19 +8,22 @@ namespace Metrics.Utils
     {
         private sealed class StopwatchClock : Clock
         {
-            private static readonly long factor = (1000L * 1000L * 1000L) / System.Diagnostics.Stopwatch.Frequency;
-            public override long Nanoseconds { get { return System.Diagnostics.Stopwatch.GetTimestamp() * factor; } }
+            private static readonly long factor = (1000L * 1000L * 1000L) / Stopwatch.Frequency;
+            public override long Nanoseconds { get { return Stopwatch.GetTimestamp() * factor; } }
+            public override DateTime LocalDateTime { get { return DateTime.Now; } }
         }
 
         private sealed class SystemClock : Clock
         {
             public override long Nanoseconds { get { return DateTime.UtcNow.Ticks * 100L; } }
+            public override DateTime LocalDateTime { get { return DateTime.Now; } }
         }
 
         public static readonly Clock SystemDateTime = new SystemClock();
         public static readonly Clock Default = new StopwatchClock();
 
         public abstract long Nanoseconds { get; }
+        public abstract DateTime LocalDateTime { get; }
 
         public long Seconds { get { return TimeUnit.Nanoseconds.ToSeconds(Nanoseconds); } }
     }
