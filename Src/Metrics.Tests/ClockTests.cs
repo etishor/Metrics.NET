4d94a04f501465445443d11cc2b2ac8d8d198b1a
Iulian Margarintescu <etishor@gmail.com>
2014-04-18 19:11:25 +0300

    add tests for clock

---
+++ Src/Metrics.Tests/ClockTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using FluentAssertions;
+using Metrics.Utils;
+using Xunit;
+
+namespace Metrics.Tests
+{
+    public class ClockTests
+    {
+        [Fact]
+        public void ClockDefaultCanMeasureTime()
+        {
+            var start = Clock.Default.Nanoseconds;
+            Thread.Sleep(20);
+            var end = Clock.Default.Nanoseconds;
+            var elapsed = TimeUnit.Nanoseconds.ToMilliseconds(end - start);
+            elapsed.Should().BeInRange(18, 22);
+        }
+
+        [Fact]
+        public void ClockSystemCanMeasureTime()
+        {
+            var start = Clock.SystemDateTime.Nanoseconds;
+            Thread.Sleep(20);
+            var end = Clock.SystemDateTime.Nanoseconds;
+            var elapsed = TimeUnit.Nanoseconds.ToMilliseconds(end - start);
+            elapsed.Should().BeInRange(18, 22);
+        }
+    }
+}
