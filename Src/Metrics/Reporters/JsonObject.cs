307a3619009722fc44a7297360c02e279dd254b8
Iulian Margarintescu <etishor@gmail.com>
2014-04-20 16:27:35 +0300

    escape newlines in json

--- Src/Metrics/Reporters/JsonObject.cs
+++ Src/Metrics/Reporters/JsonObject.cs
@@ -68,7 +68,7 @@ namespace Metrics.Reporters
 
         public static string Escape(string value)
         {
-            return value.Replace("\"", "\\\"").Replace(@"\", @"\\");
+            return value.Replace(@"\", @"\\").Replace("\"", "\\\"").Replace("\r", @"\r").Replace("\n", @"\n");
         }
     }
 
