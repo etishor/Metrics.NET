d00a2a8d1e738cfd247d294c488c56e2878147b0
Iulian Margarintescu <etishor@gmail.com>
2014-04-21 15:34:57 +0300

    bump version, push to nuget

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,2 +1,2 @@
-..\.nuget\NuGet.exe push Metrics.NET.0.1.3.nupkg
-..\.nuget\NuGet.exe push NancyFx.Metrics.0.1.3.nupkg
+..\.nuget\NuGet.exe push Metrics.NET.0.1.4.nupkg
+..\.nuget\NuGet.exe push NancyFx.Metrics.0.1.4.nupkg
