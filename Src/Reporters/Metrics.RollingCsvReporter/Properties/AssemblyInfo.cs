fb52ab6a1b5b6cc0984763fc63895978714d9cf4
nkot <nazar.kotylo@gmail.com>
2014-07-01 16:21:26 +0300

    Added MonoDebug and MonoRelease configurations for RollingCsvReporter. SharedAssemblyInfo is also reused.

--- Src/Reporters/Metrics.RollingCsvReporter/Properties/AssemblyInfo.cs
+++ Src/Reporters/Metrics.RollingCsvReporter/Properties/AssemblyInfo.cs
@@ -6,31 +6,4 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Metrics.RollingCsvReporter")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Metrics.RollingCsvReporter")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6db4ce23-5a2f-417f-9cae-a911b78f88f1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("Rolling CSV Reporter for Metrics.Net")]
