ec13ca680e5c65d93e0fc4bd1f049338fda8b38e
Iulian Margarintescu <iulian@recognos.com>
2014-07-04 17:44:18 +0300

    remove duplicate layout registration

--- Src/Adapters/Metrics.NLog/NLogReportsConfigExtensions.cs
+++ Src/Adapters/Metrics.NLog/NLogReportsConfigExtensions.cs
@@ -27,16 +27,15 @@ namespace Metrics
             return reports;
         }
 
+        /// <summary>
+        /// Write human readable text using NLog
+        /// </summary>
+        /// <param name="reports">Instance to configure</param>
+        /// <param name="interval">Interval at which to report values</param>
+        /// <returns>Same Reports instance for chaining</returns>
         public static MetricsReports WithNLogTextReports(this MetricsReports reports, TimeSpan interval)
         {
-            global::NLog.Config.ConfigurationItemFactory.Default.Layouts.RegisterDefinition("CsvGaugeLayout", typeof(CsvGaugeLayout));
-            global::NLog.Config.ConfigurationItemFactory.Default.Layouts.RegisterDefinition("CsvCounterLayout", typeof(CsvCounterLayout));
-            global::NLog.Config.ConfigurationItemFactory.Default.Layouts.RegisterDefinition("CsvMeterLayout", typeof(CsvMeterLayout));
-            global::NLog.Config.ConfigurationItemFactory.Default.Layouts.RegisterDefinition("CsvHistogramLayout", typeof(CsvHistogramLayout));
-            global::NLog.Config.ConfigurationItemFactory.Default.Layouts.RegisterDefinition("CsvTimerLayout", typeof(CsvTimerLayout));
-
             reports.WithReporter("NLog Text Report", () => new NLogTextReporter(), interval);
-
             return reports;
         }
     }
