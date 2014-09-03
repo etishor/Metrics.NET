49d0e92978ca2beb922731743091a7e8a29747f2
Iulian Margarintescu <iulian@recognos.com>
2014-09-03 16:42:33 +0300

    fix comment

--- Src/Metrics/Gauge.cs
+++ Src/Metrics/Gauge.cs
@@ -2,7 +2,7 @@
 namespace Metrics
 {
     /// <summary>
-    /// A gauge is the simplest metric type. It just returns a value.
+    /// A gauge is the simplest metric type. It just represents a value.
     /// No operation can be triggered on the metric directly. 
     /// Custom implementations can hook into any value provider.
     /// <see cref="Core.FunctionGauge"/> and <see cref="Core.DerivedGauge"/>
