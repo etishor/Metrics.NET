2ef28cba09ff534160fe18fa882950439b117d04
Iulian Margarintescu <iulian@recognos.com>
2014-09-04 17:12:02 +0300

    cleanup tests

--- Src/Metrics.Tests/DefaultMetricContextTests.cs
+++ Src/Metrics.Tests/DefaultMetricContextTests.cs
@@ -4,30 +4,36 @@ using Xunit;
 
 namespace Metrics.Tests
 {
-    public class MetricContextTests
+    public class DefaultMetricContextTests
     {
+        private readonly MetricsContext context = new DefaultMetricsContext();
+
+        public MetricsData CurrentData { get { return this.context.DataProvider.CurrentMetricsData; } }
+
         [Fact]
         public void ContextEmptyChildContextIsSameContext()
         {
-            MetricsContext context = new DefaultMetricsContext();
-
             var child = context.Context(string.Empty);
-
             object.ReferenceEquals(context, child).Should().BeTrue();
-
             child = context.Context(null);
-
             object.ReferenceEquals(context, child).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ContextChildWithSameNameAreSameInstance()
+        {
+            var first = context.Context("test");
+            var second = context.Context("test");
+
+            object.ReferenceEquals(first, second).Should().BeTrue();
         }
 
         [Fact]
         public void ContextCanCreateSubcontext()
         {
-            MetricsContext context = new DefaultMetricsContext();
-
             context.Context("test").Counter("counter", Unit.Requests);
 
-            var counterValue = context.DataProvider.CurrentMetricsData.ChildMetrics.SelectMany(c => c.Counters).Single();
+            var counterValue = CurrentData.ChildMetrics.SelectMany(c => c.Counters).Single();
 
             counterValue.Name.Should().Be("counter");
         }
@@ -35,13 +41,11 @@ namespace Metrics.Tests
         [Fact]
         public void ContextMetricsArePresentInMetricsData()
         {
-            MetricsContext context = new DefaultMetricsContext();
-
             var counter = context.Counter("test", Unit.Requests);
 
             counter.Increment();
 
-            var counterValue = context.DataProvider.CurrentMetricsData.Counters.Single();
+            var counterValue = CurrentData.Counters.Single();
 
             counterValue.Name.Should().Be("test");
             counterValue.Unit.Should().Be(Unit.Requests);
@@ -51,7 +55,6 @@ namespace Metrics.Tests
         [Fact]
         public void ContextRaisesShutdownEventOnMetricsDisable()
         {
-            MetricsContext context = new DefaultMetricsContext();
             context.MonitorEvents();
             context.CompletelyDisableMetrics();
             context.ShouldRaise("ContextShuttingDown");
@@ -60,7 +63,6 @@ namespace Metrics.Tests
         [Fact]
         public void ContextRaisesShutdownEventOnDispose()
         {
-            MetricsContext context = new DefaultMetricsContext();
             context.MonitorEvents();
             context.Dispose();
             context.ShouldRaise("ContextShuttingDown");
@@ -69,8 +71,6 @@ namespace Metrics.Tests
         [Fact]
         public void ContextDataProviderReflectsNewMetrics()
         {
-            MetricsContext context = new DefaultMetricsContext();
-
             var provider = context.DataProvider;
 
             context.Counter("test", Unit.Bytes).Increment();
@@ -81,18 +81,36 @@ namespace Metrics.Tests
         }
 
         [Fact]
+        public void ContextDataProviderReflectsChildContxts()
+        {
+            var provider = context.DataProvider;
+
+            var counter = context
+                .Context("test")
+                .Counter("test", Unit.Bytes);
+
+            counter.Increment();
+
+            provider.CurrentMetricsData.ChildMetrics.Should().HaveCount(1);
+            provider.CurrentMetricsData.ChildMetrics.Single().Counters.Should().HaveCount(1);
+            provider.CurrentMetricsData.ChildMetrics.Single().Counters.Single().Value.Should().Be(1);
+
+            counter.Increment();
+
+            provider.CurrentMetricsData.ChildMetrics.Single().Counters.Single().Value.Should().Be(2);
+        }
+
+        [Fact]
         public void ContextDisabledChildContextDoesNotShowInData()
         {
-            MetricsContext context = new DefaultMetricsContext();
-
             context.Context("test").Counter("test", Unit.Bytes).Increment();
 
-            context.DataProvider.CurrentMetricsData.ChildMetrics.Single()
+            CurrentData.ChildMetrics.Single()
                 .Counters.Single().Name.Should().Be("test");
 
             context.ShutdownContext("test");
 
-            context.DataProvider.CurrentMetricsData.ChildMetrics.Should().BeEmpty();
+            CurrentData.ChildMetrics.Should().BeEmpty();
         }
 
     }
