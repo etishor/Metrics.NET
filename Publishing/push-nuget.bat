180c83aa2539b134979369791059365f9bc71624
Iulian Margarintescu <iulian@recognos.com>
2014-09-18 10:41:57 +0300

    push 0.2.0-pre to nuget

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,3 +1,3 @@
-..\.nuget\NuGet.exe push Metrics.NET.0.1.11.nupkg
-..\.nuget\NuGet.exe push NancyFx.Metrics.0.1.11.nupkg
-..\.nuget\NuGet.exe push Owin.Metrics.0.1.2.nupkg
+..\.nuget\NuGet.exe push Metrics.NET.0.2.0-pre.nupkg
+..\.nuget\NuGet.exe push NancyFx.Metrics.0.2.0-pre.nupkg
+..\.nuget\NuGet.exe push Owin.Metrics.0.1.2-alpha.nupkg
