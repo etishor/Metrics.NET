ce7043bd4f285366f43b81e76e87e7e18ba12c33
Iulian Margarintescu <etishor@gmail.com>
2014-09-27 09:45:55 +0300

    mark NancyFx.Metrics Nuget as deprecated - use Nancy.Metrics

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,3 +1,4 @@
 ..\.nuget\NuGet.exe push Metrics.NET.0.2.1.nupkg
+..\.nuget\NuGet.exe push Nancy.Metrics.0.2.1.nupkg
 ..\.nuget\NuGet.exe push NancyFx.Metrics.0.2.1.nupkg
 ..\.nuget\NuGet.exe push Owin.Metrics.0.1.5-alpha.nupkg
