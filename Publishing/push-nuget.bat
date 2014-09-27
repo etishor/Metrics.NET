c74f259053e4d66273336c99368c7f25ea90d520
Iulian Margarintescu <etishor@gmail.com>
2014-09-27 10:02:03 +0300

    finish rename from NancyFx.Metrics to Nancy.Metrics

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,4 +1,3 @@
 ..\.nuget\NuGet.exe push Metrics.NET.0.2.1.nupkg
 ..\.nuget\NuGet.exe push Nancy.Metrics.0.2.1.nupkg
-..\.nuget\NuGet.exe push NancyFx.Metrics.0.2.1.nupkg
 ..\.nuget\NuGet.exe push Owin.Metrics.0.1.5-alpha.nupkg
