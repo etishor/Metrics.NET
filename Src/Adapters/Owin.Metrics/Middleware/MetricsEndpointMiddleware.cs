9c80f8ddeb7b9fa74fc821892ceae46e4ba1cd19
Iulian Margarintescu <etishor@gmail.com>
2014-10-05 17:40:31 +0300

    use metrics data provider instead of context in metrics endpoint

--- Src/Adapters/Owin.Metrics/Middleware/MetricsEndpointMiddleware.cs
+++ Src/Adapters/Owin.Metrics/Middleware/MetricsEndpointMiddleware.cs
@@ -18,15 +18,15 @@ namespace Owin.Metrics.Middleware
     public class MetricsEndpointMiddleware
     {
         private readonly OwinMetricsEndpointConfig endpointConfig;
-        private readonly MetricsContext context;
+        private readonly MetricsDataProvider dataProvider;
         private readonly Func<HealthStatus> healthStatus;
 
         private AppFunc next;
 
-        public MetricsEndpointMiddleware(OwinMetricsEndpointConfig endpointConfig, MetricsContext context, Func<HealthStatus> healthStatus)
+        public MetricsEndpointMiddleware(OwinMetricsEndpointConfig endpointConfig, MetricsDataProvider dataProvider, Func<HealthStatus> healthStatus)
         {
             this.endpointConfig = endpointConfig;
-            this.context = context;
+            this.dataProvider = dataProvider;
             this.healthStatus = healthStatus;
         }
 
@@ -46,7 +46,7 @@ namespace Owin.Metrics.Middleware
 
             if (requestPath.EndsWith("/" + endpointConfig.MetricsJsonEndpointName) && endpointConfig.MetricsJsonEndpointEnabled)
             {
-                return GetJsonContent(environment, this.context);
+                return GetJsonContent(environment, this.dataProvider);
             }
 
             if (requestPath.EndsWith("/" + endpointConfig.MetricsHealthEndpointName) && endpointConfig.MetricsHealthEndpointEnabled)
@@ -56,7 +56,7 @@ namespace Owin.Metrics.Middleware
 
             if (requestPath.EndsWith("/" + endpointConfig.MetricsTextEndpointName) && endpointConfig.MetricsTextEndpointEnabled)
             {
-                return GetAsHumanReadable(environment, this.context, this.healthStatus);
+                return GetAsHumanReadable(environment, this.dataProvider, this.healthStatus);
             }
 
             if (requestPath.EndsWith("/" + endpointConfig.MetricsPingEndpointName) && endpointConfig.MetricsPingEndpointEnabled)
@@ -73,9 +73,9 @@ namespace Owin.Metrics.Middleware
             return WriteResponse(environment, content, "application/json");
         }
 
-        private static Task GetJsonContent(IDictionary<string, object> environment, MetricsContext context)
+        private static Task GetJsonContent(IDictionary<string, object> environment, MetricsDataProvider dataProvider)
         {
-            var content = JsonBuilderV1.BuildJson(context.DataProvider.CurrentMetricsData, Clock.Default);
+            var content = JsonBuilderV1.BuildJson(dataProvider.CurrentMetricsData, Clock.Default);
             return WriteResponse(environment, content, "application/json");
         }
 
@@ -86,9 +86,9 @@ namespace Owin.Metrics.Middleware
             return WriteResponse(environment, content, "application/json");
         }
 
-        private static Task GetAsHumanReadable(IDictionary<string, object> environment, MetricsContext context, Func<HealthStatus> healthStatus)
+        private static Task GetAsHumanReadable(IDictionary<string, object> environment, MetricsDataProvider dataProvider, Func<HealthStatus> healthStatus)
         {
-            string text = StringReporter.RenderMetrics(context.DataProvider.CurrentMetricsData, healthStatus);
+            string text = StringReporter.RenderMetrics(dataProvider.CurrentMetricsData, healthStatus);
             return WriteResponse(environment, text, "text/plain");
         }
 
--- Src/Adapters/Owin.Metrics/OwinMetricsConfig.cs
+++ Src/Adapters/Owin.Metrics/OwinMetricsConfig.cs
@@ -65,7 +65,7 @@ namespace Owin.Metrics
         {
             OwinMetricsEndpointConfig endpointConfig = new OwinMetricsEndpointConfig();
             config(endpointConfig);
-            var metricsEndpointMiddleware = new MetricsEndpointMiddleware(endpointConfig, this.context, this.healthStatus);
+            var metricsEndpointMiddleware = new MetricsEndpointMiddleware(endpointConfig, this.context.DataProvider, this.healthStatus);
             this.middlewareRegistration(metricsEndpointMiddleware);
             return this;
         }
