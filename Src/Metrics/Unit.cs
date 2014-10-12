afd1d23c16df0fbc93c3b8f077c7b654aacd36af
Iulian Margarintescu <etishor@gmail.com>
2014-10-12 11:33:48 +0300

    refactor Unit to be a struct

--- Src/Metrics/Unit.cs
+++ Src/Metrics/Unit.cs
@@ -3,7 +3,7 @@ using System;
 using Metrics.Utils;
 namespace Metrics
 {
-    public class Unit : IHideObjectMembers
+    public struct Unit : IHideObjectMembers
     {
         public static readonly Unit None = new Unit();
         public static readonly Unit Requests = new Unit("Requests");
@@ -21,24 +21,19 @@ namespace Metrics
             return new Unit(name);
         }
 
-        private Unit()
-        {
-            this.Name = string.Empty;
-        }
+        public readonly string Name;
 
         public Unit(string name)
         {
-            if (string.IsNullOrEmpty(name))
+            if (name == null)
             {
-                throw new ArgumentException("name must not be null", "name");
+                throw new ArgumentNullException("name");
             }
 
             this.Name = name;
         }
 
-        public string Name { get; private set; }
-
-        public virtual string FormatCount(long value)
+        public string FormatCount(long value)
         {
             if (!string.IsNullOrEmpty(this.Name))
             {
@@ -47,7 +42,7 @@ namespace Metrics
             return value.ToString();
         }
 
-        public virtual string FormatValue(double value)
+        public string FormatValue(double value)
         {
             if (!string.IsNullOrEmpty(this.Name))
             {
@@ -56,21 +51,12 @@ namespace Metrics
             return value.ToString("F2");
         }
 
-        public virtual string FormatValue(string value)
-        {
-            if (!string.IsNullOrEmpty(this.Name))
-            {
-                return string.Format("{0} {1}", value, this.Name);
-            }
-            return value;
-        }
-
-        public virtual string FormatRate(double value, TimeUnit timeUnit)
+        public string FormatRate(double value, TimeUnit timeUnit)
         {
             return string.Format("{0:F2} {1}/{2}", value, this.Name, timeUnit.Unit());
         }
 
-        public virtual string FormatDuration(double value, TimeUnit? timeUnit)
+        public string FormatDuration(double value, TimeUnit? timeUnit)
         {
             return string.Format("{0:F2} {1}", value, timeUnit.HasValue ? timeUnit.Value.Unit() : this.Name);
         }
