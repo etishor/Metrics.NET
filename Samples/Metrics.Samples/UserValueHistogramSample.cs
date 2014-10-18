c40ababc0ab0fa7e00b15d8aa5eb331835116c56
Iulian Margarintescu <etishor@gmail.com>
2014-10-18 22:20:24 +0300

    add more samples

---
+++ Samples/Metrics.Samples/UserValueHistogramSample.cs
@@ -0,0 +1,30 @@
+﻿
+using Metrics.Utils;
+namespace Metrics.Samples
+{
+    public class UserValueHistogramSample
+    {
+        private readonly Histogram histogram =
+            Metric.Histogram("Results", Unit.Items);
+
+        public void Process(string documentId)
+        {
+            var results = GetResultsForDocument(documentId);
+            this.histogram.Update(results.Length, documentId);
+        }
+
+        private int[] GetResultsForDocument(string documentId)
+        {
+            return new int[ThreadLocalRandom.NextLong() % 100];
+        }
+
+        public static void RunSomeRequests()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                var documentId = ThreadLocalRandom.NextLong() % 10;
+                new UserValueHistogramSample().Process("document-" + documentId.ToString());
+            }
+        }
+    }
+}
