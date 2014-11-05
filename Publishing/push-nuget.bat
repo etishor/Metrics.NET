aa3cca0d4bf1bf9bd98aa85e1f649215c188ee6f
Iulian Margarintescu <iulian@recognos.com>
2014-11-05 20:05:07 +0200

    bump version, push to nuget

--- Publishing/push-nuget.bat
+++ Publishing/push-nuget.bat
@@ -1,3 +1,3 @@
-..\.nuget\NuGet.exe push Metrics.NET.0.2.8.nupkg
-..\.nuget\NuGet.exe push Nancy.Metrics.0.2.8.nupkg
-..\.nuget\NuGet.exe push Owin.Metrics.0.1.12-alpha.nupkg
+..\.nuget\NuGet.exe push Metrics.NET.0.2.9.nupkg
+..\.nuget\NuGet.exe push Nancy.Metrics.0.2.9.nupkg
+..\.nuget\NuGet.exe push Owin.Metrics.0.1.13-alpha.nupkg
