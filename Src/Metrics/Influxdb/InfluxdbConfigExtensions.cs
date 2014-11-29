bcac5548f07e02685855d8ea2d5e17b0882ed75f
Iulian Margarintescu <etishor@gmail.com>
2014-11-29 18:08:47 +0200

    add alternative influxdb config method

--- Src/Metrics/Influxdb/InfluxdbConfigExtensions.cs
+++ Src/Metrics/Influxdb/InfluxdbConfigExtensions.cs
@@ -6,6 +6,11 @@ namespace Metrics
 {
     public static class InfluxdbConfigExtensions
     {
+        public static MetricsReports WithInfluxDb(this MetricsReports reports, string host, int port, string user, string pass, string database, TimeSpan interval)
+        {
+            return reports.WithInfluxDb(new Uri(string.Format(@"http://{0}:{1}/db/{2}/series?u={3}&p={4}&time_precision=s", host, port, user, pass, database)), interval);
+        }
+
         public static MetricsReports WithInfluxDb(this MetricsReports reports, Uri influxdbUri, TimeSpan interval)
         {
             return reports.WithReport(new InfluxdbReport(influxdbUri), interval);
