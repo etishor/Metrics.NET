6fa5f60f6a8853ae8b6fd99b9861c5e16a58a0f9
Allan hardy <al_hardy@live.com.au>
2014-12-04 21:05:06 +1100

    format influxdb url correctly

--- Src/Metrics/Influxdb/InfluxdbConfigExtensions.cs
+++ Src/Metrics/Influxdb/InfluxdbConfigExtensions.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Metrics.Influxdb;
+﻿using Metrics.Influxdb;
 using Metrics.Reports;
+using System;
 
 namespace Metrics
 {
@@ -8,7 +8,7 @@ namespace Metrics
     {
         public static MetricsReports WithInfluxDb(this MetricsReports reports, string host, int port, string user, string pass, string database, TimeSpan interval)
         {
-            return reports.WithInfluxDb(new Uri(string.Format(@"http://{0}:{1}/db/{2}/series?u={3}&p={4}&time_precision=s", host, port, user, pass, database)), interval);
+            return reports.WithInfluxDb(new Uri(string.Format(@"http://{0}:{1}/db/{2}/series?u={3}&p={4}&time_precision=s", host, port, database, user, pass)), interval);
         }
 
         public static MetricsReports WithInfluxDb(this MetricsReports reports, Uri influxdbUri, TimeSpan interval)
