5d00e8d6ed9ea6cfa99a0b4fce3b541eb50e7b4b
Iulian Margarintescu <iulian@recognos.com>
2014-12-15 12:21:41 +0200

    fix build script

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 .nuget\NuGet.exe restore Metrics.sln
 
 set MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
-set XUNIT=".\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+set XUNIT=".\packages\xunit.runners.2.0.0-beta5-build2785\tools\xunit.console.exe"
 
 rd /S /Q .\bin\Debug
 rd /S /Q .\bin\Release
