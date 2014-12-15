0064c0da0e17aa4bd7a82408f36bd70b88b02e4d
Iulian Margarintescu <iulian@recognos.com>
2014-12-15 12:52:11 +0200

    bump version, push nuget

--- build.net40.bat
+++ build.net40.bat
@@ -1,7 +1,8 @@
 .nuget\NuGet.exe restore Metrics.sln
 
 set MSBUILD="c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
-set XUNIT=".\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+set XUNIT=".\packages\xunit.runners.2.0.0-beta5-build2785\tools\xunit.console.exe"
+
 
 %MSBUILD% Metrics.Sln /p:Configuration="Debug"
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -9,8 +10,8 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 %MSBUILD% Metrics.Sln /p:Configuration="Release"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%XUNIT% .\bin\Debug\Tests\Metrics.Tests.dll
+%XUNIT% .\bin\Debug\Tests\Metrics.Tests.dll -parallel none
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%XUNIT% .\bin\Release\Tests\Metrics.Tests.dll
+%XUNIT% .\bin\Release\Tests\Metrics.Tests.dll -parallel none
 if %errorlevel% neq 0 exit /b %errorlevel%
