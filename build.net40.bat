d3f259c029db0283f02706c5666cb21ce13737ba
Iulian Margarintescu <iulian@recognos.com>
2014-12-17 16:46:46 +0200

    fix build script

--- build.net40.bat
+++ build.net40.bat
@@ -1,7 +1,7 @@
 .nuget\NuGet.exe restore Metrics.sln
 
 set MSBUILD="c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
-set XUNIT=".\packages\xunit.runners.2.0.0-beta5-build2785\tools\xunit.console.exe"
+set XUNIT=".\packages\xunit.runners.1.9.2\\tools\xunit.console.clr4.exe"
 
 
 %MSBUILD% Metrics.Sln /p:Configuration="Debug"
@@ -10,8 +10,8 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 %MSBUILD% Metrics.Sln /p:Configuration="Release"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%XUNIT% .\bin\Debug\Tests\Metrics.Tests.dll -parallel none
+%XUNIT% .\bin\Debug\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%XUNIT% .\bin\Release\Tests\Metrics.Tests.dll -parallel none
+%XUNIT% .\bin\Release\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
