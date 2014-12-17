ccfb743c04197b9f5587a6e385f0c8adeda71e1d
Iulian Margarintescu <iulian@recognos.com>
2014-12-17 15:05:32 +0200

    revert ti xunit 1.9 (2.0 fails on travis & appveyor)

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 .nuget\NuGet.exe restore Metrics.sln
 
 set MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
-set XUNIT=".\packages\xunit.runners.2.0.0-beta5-build2785\tools\xunit.console.exe"
+set XUNIT=".\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
 
 rd /S /Q .\bin\Debug
 rd /S /Q .\bin\Release
@@ -12,8 +12,8 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 %MSBUILD% Metrics.Sln /p:Configuration="Release"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%XUNIT% .\bin\Debug\Tests\Metrics.Tests.dll -parallel none
+%XUNIT% .\bin\Debug\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%XUNIT% .\bin\Release\Tests\Metrics.Tests.dll -parallel none
+%XUNIT% .\bin\Release\Tests\Metrics.Tests.dll
 if %errorlevel% neq 0 exit /b %errorlevel%
