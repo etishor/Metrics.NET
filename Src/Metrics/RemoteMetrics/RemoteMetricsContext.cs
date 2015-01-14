84fdb9c760ce8a3376d40017985cde4381b3fc73
Iulian Margarintescu <iulian@recognos.com>
2015-01-14 16:43:22 +0200

    minor resharper suggested cleanups

--- Src/Metrics/RemoteMetrics/RemoteMetricsContext.cs
+++ Src/Metrics/RemoteMetrics/RemoteMetricsContext.cs
@@ -21,7 +21,7 @@ namespace Metrics.RemoteMetrics
         public RemoteMetricsContext(Scheduler scheduler, Uri remoteUri, TimeSpan updateInterval, Func<string, JsonMetricsContext> deserializer)
         {
             this.scheduler = scheduler;
-            this.scheduler.Start(updateInterval, (c) => UpdateMetrics(remoteUri, deserializer, c));
+            this.scheduler.Start(updateInterval, c => UpdateMetrics(remoteUri, deserializer, c));
         }
 
         private async Task UpdateMetrics(Uri remoteUri, Func<string, JsonMetricsContext> deserializer, CancellationToken token)
