a7671d987dfa2c33ce2d9ebb652fe5d6e424e203
nkot <nazar.kotylo@gmail.com>
2015-02-11 12:59:39 +0200

    Reformat code to use 4 spaces indent instead of 2

--- Src/Metrics.Tests/MetricsConfigTests/HttpEndpointTests.cs
+++ Src/Metrics.Tests/MetricsConfigTests/HttpEndpointTests.cs
@@ -10,64 +10,64 @@ using System.Collections;
 
 namespace Metrics.Tests.MetricsConfigTests
 {
-  public class HttpEndpointTests
-  {
-    private int GetUsedPort()
+    public class HttpEndpointTests
     {
-      IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
-      TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
-      IEnumerator myEnum = tcpConnInfoArray.GetEnumerator();
+        private int GetUsedPort()
+        {
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+            IEnumerator myEnum = tcpConnInfoArray.GetEnumerator();
 
-      while (myEnum.MoveNext())
-      {
-        TcpConnectionInformation TCPInfo = (TcpConnectionInformation)myEnum.Current;
-        return TCPInfo.LocalEndPoint.Port;
-      }
-      throw new Exception("No used port found.");
+            while (myEnum.MoveNext())
+            {
+                TcpConnectionInformation TCPInfo = (TcpConnectionInformation)myEnum.Current;
+                return TCPInfo.LocalEndPoint.Port;
+            }
+            throw new Exception("No used port found.");
+        }
+
+        [Fact]
+        public void HttpEndpointCanBeDisposed()
+        {
+            var config = Metric.Config.WithHttpEndpoint("http://localhost/metricstest/HttpListenerTests/HttpEndpointCanBeDisposed/");
+            config.Dispose();
+        }
+
+        [Fact]
+        public void HttpEndpointDoesNotThrowIfPortIsOccupied()
+        {
+            var usedPort = GetUsedPort();
+            Metric.Config.WithHttpEndpoint(String.Format("http://localhost:{0}/", usedPort));
+        }
+
+        [Fact]
+        public void HttpEndportLogsAnErrorIfPortIsOccupied()
+        {
+            var loggedAnError = false;
+            var config = Metric.Config;
+            config.WithErrorHandler((exception, s) => { loggedAnError = true; }, true);
+            config.WithErrorHandler((exception) => { loggedAnError = true; }, true);
+
+            var usedPort = GetUsedPort();
+            config.WithHttpEndpoint(String.Format("http://localhost:{0}/", usedPort));
+            Assert.True(loggedAnError);
+        }
+
+        [Fact]
+        public void SecondCallToWithHttpEndportDoesNotThrow()
+        {
+            var config = Metric.Config.WithHttpEndpoint("http://localhost/metricstest/HttpListenerTests/sameendpoint/");
+            config.WithHttpEndpoint("http://localhost/metricstest/HttpListenerTests/sameendpoint/");
+        }
+
+        [Fact]
+        public void HttpEndpointDoesNotThrowOnDispose()
+        {
+            var usedPort = GetUsedPort();
+
+            var config = Metric.Config.WithHttpEndpoint(String.Format("http://localhost:{0}/", usedPort));
+            config.Dispose();
+        }
+
     }
-
-    [Fact]
-    public void HttpEndpointCanBeDisposed()
-    {
-      var config = Metric.Config.WithHttpEndpoint("http://localhost/metricstest/HttpListenerTests/HttpEndpointCanBeDisposed/");
-      config.Dispose();
-    }
-
-    [Fact]
-    public void HttpEndpointDoesNotThrowIfPortIsOccupied()
-    {
-      var usedPort = GetUsedPort();
-      Metric.Config.WithHttpEndpoint(String.Format("http://localhost:{0}/", usedPort));
-    }
-
-    [Fact]
-    public void HttpEndportLogsAnErrorIfPortIsOccupied()
-    {
-      var loggedAnError = false;
-      var config = Metric.Config;
-      config.WithErrorHandler((exception, s) => { loggedAnError = true; }, true);
-      config.WithErrorHandler((exception) => { loggedAnError = true; }, true);
-
-      var usedPort = GetUsedPort();
-      config.WithHttpEndpoint(String.Format("http://localhost:{0}/", usedPort));
-      Assert.True(loggedAnError);
-    }
-
-    [Fact]
-    public void SecondCallToWithHttpEndportDoesNotThrow()
-    {
-      var config = Metric.Config.WithHttpEndpoint("http://localhost/metricstest/HttpListenerTests/sameendpoint/");
-      config.WithHttpEndpoint("http://localhost/metricstest/HttpListenerTests/sameendpoint/");
-    }
-
-    [Fact]
-    public void HttpEndpointDoesNotThrowOnDispose()
-    {
-      var usedPort = GetUsedPort();
-
-      var config = Metric.Config.WithHttpEndpoint(String.Format("http://localhost:{0}/", usedPort));
-      config.Dispose();
-    }
-
-  }
 }
