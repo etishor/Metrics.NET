747ed68a35ee7df54889d7803f7254f0ed90094b
Iulian Margarintescu <etishor@gmail.com>
2015-04-05 21:02:57 +0300

    add helper console application  for testing stuff

---
+++ Src/Metrics.TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using Metrics.Sampling;
+using Metrics.Utils;
+
+namespace Metrics.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metric.Config.WithHttpEndpoint(@"http://localhost:1234/");
+
+            var maxValue = TimeUnit.Hours.Convert(TimeUnit.Nanoseconds, 1);
+            var histogram = new HdrHistogram.NET.Histogram(maxValue, 3);
+
+            var p5 = histogram.getValueAtPercentile(50);
+
+            Reservoir reservoir = new SyncronizedHdrReservoir(histogram);
+
+            var hdr = Metric.Advanced.Histogram("hdr", Unit.Calls, () => reservoir);
+
+
+
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
