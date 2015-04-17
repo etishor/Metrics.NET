9e9dd3577db6d2ccea6c49634038f585bead1483
Iulian Margarintescu <iulian.margarintescu@recognos.ro>
2015-04-17 12:46:56 +0300

    add sample for aspnet hosting with owin

---
+++ Samples/Metrics.AspSample/AppStartup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Owin.Metrics;
+
+[assembly: OwinStartup(typeof(Metrics.AspSample.AppStartup))]
+
+namespace Metrics.AspSample
+{
+    public class AppStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Metric.Config
+                .WithOwin(m => app.Use(m), c => c.WithMetricsEndpoint());
+
+            app.UseWelcomePage();
+
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
