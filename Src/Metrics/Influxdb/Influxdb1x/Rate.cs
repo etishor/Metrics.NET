4901de1881c2945e73adf453964578bd90e14fbd
shamork <shamork@163.com>
2017-01-07 22:10:56 +0800

    update influxdb support to 1.0x All related code are copied from https://github.com/mnadel/Metrics.NET.InfluxDB/

---
+++ Src/Metrics/Influxdb/Influxdb1x/Rate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Polly;
+
+namespace Metrics.NET.InfluxDB
+{
+    internal class Rate
+    {
+        internal int Events { get; private set; }
+
+        internal TimeSpan Period { get; private set; }
+
+        internal Rate (int events, TimeSpan period)
+        {
+            Events = events;
+            Period = period;
+        }
+
+        /// <summary>
+        /// Rate specification in the form of: <code>events / timeframe</code>
+        /// </summary>
+        internal Rate (string specification)
+        {
+            var parts = specification.Split ('/');
+            Events = int.Parse (parts [0].Trim ());
+            Period = TimeSpan.Parse (parts [1].Trim ());
+        }
+
+        internal Policy AsPolicy ()
+        {
+            return Policy.Handle<Exception> ().CircuitBreaker (Events, Period);
+        }
+    }
+}
